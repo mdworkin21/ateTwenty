edab2786551073bb15bfaf21a4f415a43da96aab
Matt Dworkin <mdworkin21@gmail.com>
2018-09-18 16:18:53 -0400

    setting up server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
+//Logging Middleware
+app.use(morgan('dev'))
+
+//BodyParsing Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+//Static Middleware
+app.use(express.static(path.join(__dirname, '...', '/public')))
+
+//Route to APIs
+app.use('/api', require('./api'))
+
+//Static HTML For When No API Route Matches (Do I need this?)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '...', '/public'))
+})
+
+//Handles 500 Errs
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal Server Err. Woops!')
+})
+
+module.exports = app
