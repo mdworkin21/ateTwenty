2ae7caf8a03c2ce39f1fba300947b63d914b5155
Matt Dworkin <mdworkin21@gmail.com>
2018-09-19 12:44:01 -0400

    everything almost set up for boilerplate

---
+++ client/components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Homepage extends React.Component {
+  render(){
+    return (
+      <h1>Welcome Page</h1>
+    )
+  }
+}
+
+export default Homepage
---
+++ client/index.js
@@ -0,0 +1,9 @@
+//Root. Call ReactDOM.Render and serviceWorker here.
+//React Router stuff here
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Homepage from './components/Homepage'
+// import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<Homepage />, document.getElementById('root') )
