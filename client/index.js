f8143cab0409d760508f2ace8023b66b5ceb131b
Matt Dworkin <mdworkin21@gmail.com>
2018-09-19 15:05:50 -0400

    boiler plate set up

--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
 //Root. Call ReactDOM.Render and serviceWorker here.
 //React Router stuff here
-
+//Set up Provider for Redux
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Homepage from './components/Homepage'
 import store from './store'
+import {Provider} from 'react-redux'
 // import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
