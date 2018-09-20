ac3f23f95498e7086a35831723c5c3eadfdb3674
Matt Dworkin <mdworkin21@gmail.com>
2018-09-20 17:12:05 -0400

    icons are links in nav bar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import {Menu} from 'semantic-ui-react'
+import {Menu, Icon, Item} from 'semantic-ui-react'
 
-const NavBar = () => {
-  return (
-    <Menu>
-        <Link to="/" id="homeLink" className="link">Home</Link>
-        <Link to="/calc" id="searchLink" className="link">Calc</Link>
-        <Link to="/log" id="searchLink" className="link">Log</Link>
-    </Menu>
-  )
+class NavBar extends Component {
+  constructor(){
+    super()
+    this.state = {
+      link: ''
+    }
+  }
+
+  render(){
+    
+
+    return (
+      <Menu icon>
+        <Menu.Item name='home'> 
+          <Link to="/"> <Icon name='home'/></Link>
+        </Menu.Item>
+
+        <Menu.Item name='calculator'> 
+        <Link to="/calc"> <Icon name='calculator'/></Link>
+        </Menu.Item>
+
+        <Menu.Item name='list alternate outline'> 
+          <Link to="/log"><Icon name='list alternate outline'/></Link>
+        </Menu.Item>
+      </Menu>
+    )
+  }
 }
 
-export default NavBar
+export default NavBar 
